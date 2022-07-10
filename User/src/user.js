3733d3fa0ca3935c6f9b7e7a48f838b5c439e7a8
duygucaner <105943152+duygucaner@users.noreply.github.com>
2022-07-10 22:41:45 +0300

    Add files via upload

---
+++ User/src/user.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Table } from "react-bootstrap";
+
+export default class User extends Component {
+    state = {
+        user: [],
+      };
+    
+      componentDidMount() {
+        const url = "https://jsonplaceholder.typicode.com/users";
+        axios.get(`${url}`).then((res) => {
+          const user = res.data;
+          this.setState({
+            user: user,
+          });
+        });
+      }
+    
+      render() {
+        return this.state.user.map((user) => (
+          <div className="app">
+          <Table>
+              <tbody className="table">
+                <tr>{user.name}</tr>
+              </tbody>
+          </Table>
+          </div>
+        ));
+      }
+}
