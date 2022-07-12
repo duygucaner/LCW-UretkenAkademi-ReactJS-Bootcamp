b50c008712930a18c47c02ed4da5b9b889de761a
duygucaner <105943152+duygucaner@users.noreply.github.com>
2022-07-13 02:43:11 +0300

    Add files via upload

---
+++ Color Generator/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f5f3f2').all(10))
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <h3 className='baslikmuf'>color generator</h3>
+      <section className='container'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f5f3f2'
+            className={`${error ? 'error' : null}`}
+          />
+        </form>
+        <button className='btn' onClick={handleSubmit} >
+            submit
+          </button>
+        <p></p>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
+}
+
+export default App
